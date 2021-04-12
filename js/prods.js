7d65ce9853e48d83b7fcf0611a2ace3886656a47
Marcos Paranhos <mark.paranhoss@gmail.com>
2021-04-12 11:36:24 -0300

    error correction and finalization

--- js/prods.js
+++ js/prods.js
@@ -2,6 +2,13 @@
 
 const tbodyList = document.getElementById('tbodyList');
 
+const lblCod = document.getElementById('lblCod');
+const inpNome = document.getElementById('inpNome');
+const inpDesc = document.getElementById('inpDesc');
+const inpQtda = document.getElementById('inpQtda');
+const inpFab = document.getElementById('inpFab');
+const btnAlterar = document.getElementById('btnAlterar');
+
 const popUp = document.querySelector('.popUpWrapper');
 
 
@@ -19,7 +26,7 @@ function consultaGeral(){
         tbodyList.innerHTML='';
         for (i = 0; i < data.length; i++){
             tr = '<tr>' +
-                    '<td>' + data[i].code + '</td>' +
+                    '<td>' + data[i].cod + '</td>' +
                     '<td>' + data[i].nome + '</td>' +
                     '<td>' + data[i].descri + '</td>' +
                     '<td>' + data[i].qtda + '</td>' +
@@ -32,19 +39,53 @@ function consultaGeral(){
     })
 }
 
-function onEdit(){
+function onEdit(td){
+    let dataSelection = td.parentElement.parentElement;
+    
     popUp.style.display= 'block'
+    lblCod.innerHTML = dataSelection.cells[0].innerHTML;
+    console.log(dataSelection);
+    inpNome.value = dataSelection.cells[1].innerHTML;
+    inpDesc.value = dataSelection.cells[2].innerHTML;
+    inpQtda.value = dataSelection.cells[3].innerHTML;
+    inpFab.value = dataSelection.cells[4].innerHTML;
+}
+
+function updateProds(){
+    let codProd = lblCod.innerHTML;
+    let nomeProd = inpNome.value;
+    let descProd = inpDesc.value;
+    let qtdaProd = inpQtda.value;
+    let fabProd = inpFab.value;
+
+    data = {
+        'nome': nomeProd,
+        'descri': descProd,
+        'qtda': qtdaProd,
+        'fabricante': fabProd
+    };
+
+    console.log('Código do produto = ' + codProd);
+
+    api.put('produtos/' + codPro, data).then(res=>{
+        console.log('Alteração realizada!');
+        consultaGeral();
+    }).catch(error=> console.log('Erro ao realizar a alteração'));
+}
+
+btnAlterar.onclick = ()=>{
+    updateProds();
 }
 
 popUp.addEventListener('click', event =>{
     const classClick = event.target.classList[0];
-    console.log(classClick);
+    //console.log(classClick); //Show the class clicked
     if (classClick === 'popUpClose' ||
             classClick === 'closeLinkPopUp')
             {
                 popUp.style.display = 'none' 
             }
     
-})
+});
 
 consultaGeral();
